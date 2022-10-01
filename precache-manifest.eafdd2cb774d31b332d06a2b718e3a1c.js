d78550eca8f0900781b485db6631e93db9999047
juhwan976 <juhwan976@gmail.com>
2022-10-01 14:03:52 +0900

    Updates

---
+++ precache-manifest.eafdd2cb774d31b332d06a2b718e3a1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "633f5dca3017fa88794f",
+    "url": "/static/css/main.b47832ff.chunk.css"
+  },
+  {
+    "revision": "633f5dca3017fa88794f",
+    "url": "/static/js/main.007c07a4.chunk.js"
+  },
+  {
+    "revision": "32b369d30a5f68a92360",
+    "url": "/static/js/runtime~main.aea6f749.js"
+  },
+  {
+    "revision": "7a1a5f510c37ac1d83e2",
+    "url": "/static/js/2.cda36770.chunk.js"
+  },
+  {
+    "revision": "e8509cf3d4567de2286b",
+    "url": "/static/js/3.de8fb8d5.chunk.js"
+  },
+  {
+    "revision": "35f42dd08f5478ba228d678712b9c755",
+    "url": "/static/media/LeferiPointWhite.35f42dd0.ttf"
+  },
+  {
+    "revision": "23fc4cd7dabb95ade4c378922727f1e3",
+    "url": "/index.html"
+  }
+];
